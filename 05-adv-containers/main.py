b761fc1f75c694bdb425adb24b0da37a56629966
John Hunt <john.hunt10@gmail.com>
2023-02-24 08:56:34 +0000

    Refactored to focus on dictionary

--- 05-adv-containers/main.py
+++ 05-adv-containers/main.py
@@ -9,79 +9,15 @@ MAX_VALUE = 10
 MAX_NUMBER_OF_GUESSES = 4
 GUESS_PROMPT = 'Please guess a number between ' + str(MIN_VALUE) + ' and ' + str(MAX_VALUE) + ': '
 
-# Set up global variables
-# Low score dictionary
-low_score_dictionary = {}
 # current user
 current_user = None
 
+# Low score dictionary
+low_score_dictionary = {}
 
-def welcome_message():
-    print('Welcome to the number guess game')
-
-
-def get_user_yes_or_no(prompt):
-    """ get input from the user and check that it is y or n"""
-    invalid_input = True
-    while invalid_input:
-        user_input = input(prompt).lower()
-        if user_input == 'y' or user_input == 'n':
-            return user_input
-        else:
-            print('Input Error - Input must be "y" or "n"')
-
-
-def display_instructions():
-    response = get_user_yes_or_no('Do you want to see the instructions?: ')
-    if response == 'y':
-        print(GUESS_PROMPT)
-        print("You can play as many times as you like")
-
-
-def game_over_message():
-    print('Game Over')
-    print('Low Score table:')
-    for key in low_score_dictionary:
-        print(f'\t{key} = {low_score_dictionary[key]}')
-
-
-def check_current_user():
-    global current_user
-    check_new_user = 'n'
-    if current_user is not None:
-        user_prompt = f'The current user is {current_user}, do you wish to enter a new name (y/n): '
-        check_new_user = input(user_prompt).lower()
-
-    if check_new_user == 'y' or current_user is None:
-        current_user = input('Please input your name: ')
-
-
-def get_user_guess(prompt):
-    user_input_int = None
-    invalid_input = True
-    while invalid_input:
-        user_input = input(prompt)
-        if user_input == '-1':
-            return -1
-        if not user_input.isdigit():
-            print('Input must be a positive number')
-        else:
-            user_input_int = int(user_input)
-            if user_input_int < MIN_VALUE or user_input_int > MAX_VALUE:
-                print(f'Number must be in range {MIN_VALUE} and {MAX_VALUE}')
-            else:
-                invalid_input = False
-    return user_input_int
-
-
-def update_low_score_table(user, count_number_of_tries):
-    previous_low_score = low_score_dictionary.get(user, 1000)
-    if previous_low_score > count_number_of_tries:
-        low_score_dictionary[current_user] = count_number_of_tries
-
-
-# Set up main game loop variable
+# Set up variables to be used in the game
 game_over = False
+
 while not game_over:
 
     # Initialise the history of guesses
@@ -94,11 +30,15 @@ while not game_over:
     count_number_of_tries = 0
 
     # Start the game
-    welcome_message()
-    # display instructions if required
-    display_instructions()
-    # Check the current user setting
-    check_current_user()
+    print('Welcome to the number guess game')
+
+    check_new_user = 'n'
+    if current_user is not None:
+        user_prompt = f'The current user is {current_user}, do you wish to enter a new name (y/n): '
+        check_new_user = input(user_prompt).lower()
+
+    if check_new_user == 'y' or current_user is None:
+        current_user = input('Please input your name: ')
 
     # Obtain their initialise guess
     guess = 0
@@ -107,7 +47,7 @@ while not game_over:
         # Set up a message to user variable
         message_to_user = ''
 
-        guess = get_user_guess(GUESS_PROMPT)
+        guess = int(input(GUESS_PROMPT))
 
         # Check to see they have not exceeded the maximum
         # number of attempts if so break out of loop otherwise
@@ -120,7 +60,7 @@ while not game_over:
             break
         elif number_to_guess == guess:
             message_to_user = 'Correct Guess'
-        elif guess + 1 == number_to_guess or guess - 1 == number_to_guess:
+        elif guess + 1 == number_to_guess or guess -1 == number_to_guess:
             message_to_user = 'Sorry wrong number - you were out by 1'
         elif guess < number_to_guess:
             message_to_user = "Sorry wrong number\nYour guess was lower than the number"
@@ -143,7 +83,9 @@ while not game_over:
         print("Sorry - you loose")
         print(f'The number you needed to guess was {number_to_guess}')
 
-    update_low_score_table(current_user, count_number_of_tries)
+    previous_low_score = low_score_dictionary.get(current_user, 1000)
+    if previous_low_score > count_number_of_tries:
+        low_score_dictionary[current_user] = count_number_of_tries
 
     # Present the guess history
     print('Your guesses were:')
@@ -153,4 +95,7 @@ while not game_over:
     if play_again.lower() == 'n' or play_again.lower() == 'no':
         game_over = True
 
-game_over_message()
+print('Game Over')
+print('Low Score table:')
+for key in low_score_dictionary:
+    print(f'\t{key} = {low_score_dictionary[key]}')
